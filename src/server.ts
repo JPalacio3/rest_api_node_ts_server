b2f2b2ec8a20600f6d9746b17110d520f69037d4
JPalacio3 <jpalaciocano3@gmail.com>
2025-07-22 18:01:45 -0600

    feat: configuración de la base de datos-conexión desde el backend

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from "express";
 import router from "./router";
+import db from "./config/db";
 
+// Conectar a  la base de datos
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión a la base de datos");
+  } catch (error) {
+    console.error("Hubo un error al conectar a la base de datos");
+    console.error(error);
+  }
+}
+
+connectDB();
 const server = express();
 
 // Importación del router para obtener las peticiones de la API
