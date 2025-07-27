83f4579d06e457e2523ce58a1eba9aadb2e784eb
JPalacio3 <jpalaciocano3@gmail.com>
2025-07-27 17:18:45 -0600

    feat: Almacenando los datos de la consulta en la base de datos y retornando la respuesta con el objeto creado

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
+import Products from "../models/Product.model";
 
-export default function createProduct(req: Request, res: Response) {
-  console.log(req.body);
-  res.send("Hola mundo Desde POST");
-}
+export const createProduct = async (req: Request, res: Response) => {
+  // Enviar el producto a la base de datos
+  const product = await Products.create(req.body);
+
+  // Retornar el producto creado
+  res.json({ data: product });
+};
