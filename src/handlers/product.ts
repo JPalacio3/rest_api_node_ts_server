3d29d8128db2e8bf845a16c4b0f4e20ac9941c31
JPalacio3 <jpalaciocano3@gmail.com>
2025-08-09 16:06:18 -0600

    feat: Creación del Handler para obtener los registros de la base de datos (GET )

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from "express";
 import Products from "../models/Product.model";
 
+// Handler para obtener todos los productos
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Products.findAll({
+      order: [["createdAt", "DESC"]], // Ordenar por fecha de creación descendente
+    });
+    res.json({ data: products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener los productos" });
+  }
+};
+
+// Handler para guardar los productos en la base de datos
 export const createProduct = async (req: Request, res: Response) => {
   try {
     // Enviar el producto a la base de datos
@@ -9,5 +23,6 @@ export const createProduct = async (req: Request, res: Response) => {
     res.json({ data: product });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "Error al crear el producto" });
   }
 };
